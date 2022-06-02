df5c7863d33aea2eee41d754163aab626d80b18d
MAXLZ1 <MAXLZ1@users.noreply.github.com>
2022-06-02 11:05:42 +0000

    deploy: 5b8cb6c408673387503267493e3592443f5edd11

---
+++ assets/vue-router_replace.md.e6483591.lean.js
@@ -0,0 +1,1 @@
+import{_ as n,c as a,o as s,d as e}from"./app.69cbc3a8.js";const m='{"title":"replace","description":"","frontmatter":{},"headers":[],"relativePath":"vue-router/replace.md"}',t={},p=e("",4),o=[p];function c(l,r,u,i,d,k){return s(),a("div",null,o)}var h=n(t,[["render",c]]);export{m as __pageData,h as default};
