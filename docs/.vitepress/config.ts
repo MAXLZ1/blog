232b81045eade6089d8d6261c2adfc05c3700e2a
MAXLZ <2251683416@qq.com>
2022-06-09 23:12:09 +0800

    feat: add vue3的响应系统分析.md

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -57,45 +57,49 @@ export default defineConfig({
 
 function getVueSidebar() {
   return [
+    { text: '源码目录结构', link: '/vue3-analysis/structure' },
     {
-      text: 'Vue3源码解析',
+      text: '响应系统',
       children: [
-        { text: '源码目录结构', link: '/vue3-analysis/structure' },
         {
-          text: 'reactive',
-          children: [
-            { text: 'reactive', link: '/vue3-analysis/reactive/reactive' },
-            { text: 'shallowReactive', link: '/vue3-analysis/reactive/shallowReactive' },
-            { text: 'readonly', link: '/vue3-analysis/reactive/readonly' },
-            { text: 'shallowReadonly', link: '/vue3-analysis/reactive/shallowReadonly' },
-            { text: 'toRaw', link: '/vue3-analysis/reactive/toRaw' },
-          ]
-        },
-        {
-          text: 'refs',
-          children: [
-            { text: 'ref',  link: '/vue3-analysis/refs/ref' },
-            { text: 'shallowRef',  link: '/vue3-analysis/refs/shallowRef' },
-            { text: 'customRef',  link: '/vue3-analysis/refs/customRef' },
-          ]
-        },
-        {
-          text: 'effect',
-          children: [
-            { text: '依赖收集', link: '/vue3-analysis/effect/依赖收集' },
-            { text: '触发依赖', link: '/vue3-analysis/effect/触发依赖' },
-          ]
-        },
-        { text: 'computed', link: '/vue3-analysis/computed' },
-        { text: 'scheduler', link: '/vue3-analysis/scheduler/scheduler' },
-        {
-          text: 'watch',
-          children: [
-            { text: 'watch', link: '/vue3-analysis/watch/watch' },
-          ]
+          text: '副作用函数与响应式数据',
+          link: '/vue3-analysis/reactivity/副作用函数与响应式数据'
         },
       ]
-    }
+    },
+    {
+      text: 'reactive',
+      children: [
+        { text: 'reactive', link: '/vue3-analysis/reactive/reactive' },
+        { text: 'shallowReactive', link: '/vue3-analysis/reactive/shallowReactive' },
+        { text: 'readonly', link: '/vue3-analysis/reactive/readonly' },
+        { text: 'shallowReadonly', link: '/vue3-analysis/reactive/shallowReadonly' },
+        { text: 'toRaw', link: '/vue3-analysis/reactive/toRaw' },
+      ]
+    },
+    {
+      text: 'refs',
+      children: [
+        { text: 'ref',  link: '/vue3-analysis/refs/ref' },
+        { text: 'shallowRef',  link: '/vue3-analysis/refs/shallowRef' },
+        { text: 'customRef',  link: '/vue3-analysis/refs/customRef' },
+      ]
+    },
+    {
+      text: 'effect',
+      children: [
+        { text: '依赖收集', link: '/vue3-analysis/effect/依赖收集' },
+        { text: '触发依赖', link: '/vue3-analysis/effect/触发依赖' },
+      ]
+    },
+    { text: 'computed', link: '/vue3-analysis/computed' },
+    { text: 'scheduler', link: '/vue3-analysis/scheduler/scheduler' },
+    {
+      text: 'watch',
+      children: [
+        { text: 'watch', link: '/vue3-analysis/watch/watch' },
+      ]
+    },
   ]
 }
 
